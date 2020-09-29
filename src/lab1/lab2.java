aff1432751c7561e6cec0c272af5d0a1b33b9363
user <user@EC09>
2020-09-29 09:43:09 +0800

    2

---
+++ src/lab1/lab2.java
@@ -0,0 +1,13 @@
+package lab1;
+
+public class lab2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I'm Ching");
+		System.out.println("My NID is D0843778");
+		System.out.println("This is lab 1 hw");
+
+	}
+
+}
